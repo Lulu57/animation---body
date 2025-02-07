b6a82a967dabc47fa40efdca87f95648139d6f64
Lulu57 <28lclunie@aos92.org>
2025-02-07 09:49:20 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+let mySprite = sprites.create(assets.image`STICKY`, SpriteKind.Player)
+animation.runImageAnimation(
+mySprite,
+assets.animation`_THATS MY BOY- NO MY BOY_ -FATHER HELP-`,
+500,
+true
+)
