03dae186419b69704039bfe713e3f3b6d88375c9
Lulu57 <28lclunie@aos92.org>
2025-02-12 09:38:54 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,56 @@
-let mySprite = sprites.create(assets.image`STICKY`, SpriteKind.Player)
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Jumping jacks and a little victory cheer thing`,
+    500,
+    false
+    )
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Walking`,
+    500,
+    false
+    )
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Handshake`,
+    500,
+    false
+    )
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Catching and Throwing a Ball and Shrugging`,
+    500,
+    false
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Shrugging and Shifting`,
+    500,
+    false
+    )
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Victory Thing`,
+    500,
+    false
+    )
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`Original Frame`, SpriteKind.Player)
 animation.runImageAnimation(
 mySprite,
-assets.animation`_THATS MY BOY- NO MY BOY_ -FATHER HELP-`,
+assets.animation`Total Combined Animaitions`,
 500,
-true
+false
 )
